1c547f06fbcc269e7c075e28483650f6d5548680
Kirill <apuc06@mail.ru>
2016-01-15 18:31:15 +0300

    slider arrow

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
     $('.slider-box').fotorama();
+
+    $('.custom-arrow-left').on('click', function(){
+        var $fotoramaDiv = $('.slider-box').fotorama();
+        var fotorama = $fotoramaDiv.data('fotorama');
+        fotorama.show('<');
+    });
+
+    $('.custom-arrow-right').on('click', function(){
+        var $fotoramaDiv = $('.slider-box').fotorama();
+        var fotorama = $fotoramaDiv.data('fotorama');
+        fotorama.show('>');
+    });
 });
 
