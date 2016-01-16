abd8a296c26431c2dc4d0f54d265e82ab4ecd34a
AKD47 <van83horn@gmail.com>
2016-01-16 10:39:09 +0300

    изменения в товары

--- js/script.js
+++ js/script.js
@@ -2,30 +2,5 @@ $(document).ready(function(){
     $('.slider-box').fotorama();
 });
 
-$(document).ready(function(){
-    $('div.rating').rating();
-});
 
 
-$('#rating').rating({
-
-    fx: 'float',
-
-    image: 'images/stars.png',
-
-    loader: 'images/ajax-loader.gif',
-
-    minimal: 0.6,
-
-    url: 'rating.php',
-
-    callback: function (responce) {
-
-
-        this.vote_success.fadeOut(2000);
-
-        if (responce.msg) alert(responce.msg);
-
-    }
-
-});
